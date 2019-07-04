52ef1dcffe29c8b9e18e0b101437661151f950c4
zhucuicui <zhucuicui@jd.com>
2019-07-04 18:11:21 +0800

    add index

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ let fs=require('fs')
 let list=fs.readdirSync('./pages')
 //console.log(list)
 let str=list.map(v=>{
-  return `<a href="./pages/${v}" target="_blank">${v}</a>`
+  return `<li><a href="./pages/${v}" target="_blank">${v}</a></li>`
 })
 let data=`
 <html>
 <body>
+<ul>
 ${str.join(' ')}
+</ul>
 </body>
 </html>
 `
