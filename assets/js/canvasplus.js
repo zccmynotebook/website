3755fa12008fbbe54d3ad70fe3b063a382d93d9e
zhucuicui <zhucuicui@jd.com>
2019-07-16 15:55:14 +0800

    add

--- assets/js/canvasplus.js
+++ assets/js/canvasplus.js
@@ -1,6 +1,8 @@
 let txt=document.querySelector('#txt');
 let img=document.querySelector('img')
+let caption=document.querySelector('#caption')
 let preview=document.querySelector('#preview')
+let file=document.querySelector('#file')
 let save=document.querySelector("#save")
 let canvas=document.querySelector('#canvas')
 let ctx=canvas.getContext('2d');
@@ -8,11 +10,21 @@ ctx.font="16px Georgia";
 img.crossOrigin="anonymous";
 img.setAttribute("crossOrigin",'Anonymous')
 preview.onclick=function(){
+   getImg() 
+}
+save.onclick=function(){
    ctx.clearRect(0,0,100,100)
    ctx.drawImage(img,0,0,80,80)
    ctx.fillText(txt.value,12,96,100)
-}
-save.onclick=function(){
    let i=canvas.toDataURL('image/png')
    save.href=i; 
 }
+function getImg(){
+   let reader=new FileReader()
+   reader.readAsDataURL(file.files[0])
+   reader.onload=function(e){
+      img.src=e.target.result
+   }
+   caption.textContent=txt.value
+}
+ 
