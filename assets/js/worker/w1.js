db9805819d1545b0c917115874ba395e0aeb8415
zhucuicui <zhucuicui@jd.com>
2019-09-24 19:47:35 +0800

    add

--- assets/js/worker/w1.js
+++ assets/js/worker/w1.js
@@ -1,4 +1,4 @@
 self.onmessage=function(e){
   console.log('w1',e)
-  e.ports[0].postMessage('w1')
+  e.ports[0].postMessage(e.data)
 }
--- assets/js/worker/w2.js
+++ assets/js/worker/w2.js
@@ -1,7 +1,6 @@
 self.onmessage=function(e){
   console.log('w2',e)
   const port = e.ports[0];
-  // 由于在worker中无法使用console.log，因此我们通过给w2绑定onmessage回调函数来验证传递是否成功
   port.onmessage = function(e) {
       postMessage(e.data)
   }
