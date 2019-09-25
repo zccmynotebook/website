14fa06a314240af07bf237ca20315f346214e998
zhucuicui <zhucuicui@jd.com>
2019-09-25 10:56:59 +0800

    add

--- assets/js/worker/w1.js
+++ assets/js/worker/w1.js
@@ -1,4 +1,10 @@
 self.onmessage=function(e){
   console.log('w1',e)
-  e.ports[0].postMessage('from worker1---'+e.data)
+  const port = e.ports[0];
+  postMessage('w1,not port1')
+  port.postMessage('msg from worker1---'+e.data)
+  port.onmessage = function(e) {
+    postMessage(e.data)
+  }  
+  e.ports[1].postMessage('from ch1 port2')
 }
--- assets/js/worker/w2.js
+++ assets/js/worker/w2.js
@@ -1,6 +1,7 @@
 self.onmessage=function(e){
   console.log('w2',e)
   const port = e.ports[0];
+  postMessage('w2,not port2')
   port.postMessage('w2-port')
   port.onmessage = function(e) {
       postMessage(e.data)
